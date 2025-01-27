54b88e2dfacbb5b6c277807697eb2f05b415c922
woshileow <josh.leow.2010@gmail.com>
2025-01-27 15:30:33 +0800

    show YoY data

--- src/mplfinance.py
+++ src/mplfinance.py
@@ -8,33 +8,36 @@ import mplfinance as mpf
 import matplotlib.pyplot as plt
 import finplot as fplt
 
-# from src.config import DESIRED_YEAR, DESIRED_MONTH
-#
+from config import DESIRED_YEAR, DESIRED_MONTH
+
 # print("\n############# COMMAND TO KILL PROCESS: #############\n"
 #         "ps | grep mplfinance | awk '{print $1}' | xargs kill\n"
 #         "####################################################\n")
 #
-# # Load the CSV file into a DataFrame
-# csv_path = Path(os.path.join(os.getcwd(), f"data/ipo-dataset/{DESIRED_YEAR}/{DESIRED_MONTH}/ACT.csv"))
-# df = pd.read_csv(csv_path)
-#
-# # Group the rows into batches of 250
-# df['YoY'] = df.index // 250
-#
-# # Calculate the highest "High" and lowest "Low" for each batch
-# result = df.groupby('YoY').agg({
-#     'High': 'max',
-#     'Low': 'min'
-# }).reset_index()
-#
-# # Rename the columns for clarity
+# Load the CSV file into a DataFrame
+csv_path = Path(os.path.join(os.getcwd(), f"data/ipo-dataset/{DESIRED_YEAR}/{DESIRED_MONTH}/ANAB.csv"))
+df = pd.read_csv(csv_path)
+
+# Group the rows into batches of 250 (Year on Year)
+df['YoY'] = df.index // 250
+print(df)
+
+# Aggregate required values for each batch
+result = df.groupby('YoY').agg({
+    'High': 'max',  # Highest "High"
+    'Low': 'min',   # Lowest "Low"
+    'Open': 'first',  # First day's "Open"
+    'Close': 'last'   # Last day's "Close"
+}).reset_index()
+
+# Rename the columns for clarity
 # result.rename(columns={'High': 'Highest High', 'Low': 'Lowest Low'}, inplace=True)
-#
+
 # print(result)
 
 
-df = yf.download('ACT')
-fplt.candlestick_ochl(df[['Open','Close','High','Low']])
-fplt.plot(df.Close.rolling(50).mean())
-fplt.plot(df.Close.rolling(200).mean())
+# df = yf.download('ACT')
+fplt.candlestick_ochl(result[['Open','Close','High','Low']])
+# fplt.plot(df.Close.rolling(50).mean())
+# fplt.plot(df.Close.rolling(200).mean())
 fplt.show()
